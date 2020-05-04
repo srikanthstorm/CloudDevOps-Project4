7fc10c2d4c2384ed3a7249fc3f3b059ecc302677
srikanthstorm <srikanthnaidu512@gmail.com>
2020-05-04 17:22:37 +0530

    feat: Docker file added

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+export DOCKER_PATH=fabiotavarespr/udacity-devopsenginner-project4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run --generator=run-pod/v1 udacity-project4 --image=$DOCKER_PATH --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/udacity-project4 8000:80
